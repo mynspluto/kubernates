7b7a26dc2151f374a15449eb9f7b2fac5489eb6d
mynspluto <mynspluto@gmail.com>
2024-05-02 15:30:17 +0900

    service로 pod간 연결 안 되는 이슈 해결

--- expressAndDBEachPod/README.md
+++ expressAndDBEachPod/README.md
@@ -1,11 +1,15 @@
 cd exress
-docker build -t express_db_each_pod:latest .
-minikube image load express_db_each_pod
+docker build -t express_db_each_pod:latest . --no-cache
+minikube image load express_db_each_pod:latest
 
-kubectl apply -f deployment-express.yml
+kubectl get deployment
+kubectl delete deployment express-deployment
+kubectl delete deployment maria-deployment
+
 kubectl apply -f deployment-maria.yml
-kubectl apply -f service-express.yml
 kubectl apply -f service-maria.yml
+kubectl apply -f deployment-express.yml
+kubectl apply -f service-express.yml
 
 kubectl port-forward service/express-service 31000
 (minikube service express-service --url 이것도 가능)
@@ -13,8 +17,16 @@ kubectl port-forward service/express-service 31000
 minikube addons list | grep ingress
 minikube addons enable ingress
 minikube addons enable ingress-dns
+
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+=> 에러시 유효성 검증 제거
 kubectl apply -f ingress-express.yml
 
 sudo vi /etc/hosts
 => 127.0.0.1 example.com
 minikube tunnel
+example.com 접속
+
+service로 pod간 continer간 연결이 안되는 경우 coredns가 kube-system에 있는지 확인
+=> kubectl get pod -n kube-system
+없는 경우 minikube stop, minikube delete, minikube start로 coredns가 생기게 유도
--- expressAndDBEachPod/express/routes/index.js
+++ expressAndDBEachPod/express/routes/index.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const mysql = require("mysql2");
 
-const maria = require("../maria"); //maria.js 경로 입력
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "maria-service",
+  port: "32000",
+  // host: "127.0.0.1",
+  // port: "3306",
+  user: "root",
+  password: "dbslzhs90!",
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -12,13 +21,20 @@ router.get("/select", async (req, res, next) => {
   initializeDatabase()
     .then(() => {
       console.log("Database initialization completed.");
-      maria.query("select * from my_table", function (err, rows, fields) {
-        if (!err) {
-          console.log("succ", rows);
-          res.render("select", { data: rows });
-        } else {
-          console.log("err : ", err);
-        }
+      pool.getConnection((err, connection) => {
+        connection.query("USE kubernetes_express; ", () => {
+          connection.query(
+            "select * from my_table",
+            function (err, rows, fields) {
+              if (!err) {
+                console.log("succ", rows);
+                res.render("select", { data: rows });
+              } else {
+                console.log("err : ", err);
+              }
+            }
+          );
+        });
       });
     })
     .catch((err) => {
@@ -28,59 +44,73 @@ router.get("/select", async (req, res, next) => {
 
 async function createDatabase() {
   return new Promise((resolve, reject) => {
-    maria.query(
-      "CREATE DATABASE IF NOT EXISTS kubernetes_express;",
-      (err, result) => {
-        if (err) {
-          console.error("Error creating database:", err);
-          reject(err);
-          return;
-        }
-        console.log("Database created successfully.");
-        resolve();
+    console.log("createDatabase");
+    console.log("pool", pool);
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log("getConnection error", err);
+      } else {
+        console.log("connection", connection);
+        connection.query(
+          "CREATE DATABASE IF NOT EXISTS kubernetes_express;",
+          (err, result) => {
+            if (err) {
+              console.error("Error creating database:", err);
+              reject(err);
+              return;
+            }
+            console.log("Database created successfully.");
+            resolve();
+          }
+        );
       }
-    );
+    });
   });
 }
 
 // 테이블 생성 및 데이터 삽입 함수
 async function createTableAndInsertData() {
   return new Promise((resolve, reject) => {
-    maria.query("USE kubernetes_express;", (err, result) => {
-      if (err) {
-        console.error("Error using database:", err);
-        reject(err);
-        return;
-      }
-
-      // 테이블 생성 쿼리
-      maria.query(
-        "CREATE TABLE IF NOT EXISTS my_table (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));",
-        (err, result) => {
-          if (err) {
-            console.error("Error creating table:", err);
-            reject(err);
-            return;
-          }
-
-          // 테스트 데이터 삽입 쿼리
-          maria.query(
-            "INSERT INTO my_table (name) VALUES ('John Doe');",
-            (err, result) => {
-              if (err) {
-                console.error("Error inserting test data:", err);
-                reject(err);
-                return;
-              }
-
-              console.log(
-                'Table "my_table" created and test data inserted successfully!'
-              );
-              resolve();
-            }
-          );
+    console.log("createTableAndInsertData");
+    console.log("pool", pool);
+    pool.getConnection((err, connection) => {
+      console.log("connection", connection);
+      connection.query("USE kubernetes_express;", (err, result) => {
+        if (err) {
+          console.error("Error using database:", err);
+          reject(err);
+          return;
         }
-      );
+
+        // 테이블 생성 쿼리
+        connection.query(
+          "CREATE TABLE IF NOT EXISTS my_table (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));",
+          (err, result) => {
+            if (err) {
+              console.error("Error creating table:", err);
+              reject(err);
+              return;
+            }
+
+            // 테스트 데이터 삽입 쿼리
+            connection.query(
+              "INSERT INTO my_table (name) VALUES ('John Doe');",
+              (err, result) => {
+                if (err) {
+                  console.error("Error inserting test data:", err);
+                  reject(err);
+                  return;
+                }
+
+                console.log(
+                  'Table "my_table" created and test data inserted successfully!'
+                );
+                resolve();
+              }
+            );
+          }
+        );
+      });
     });
   });
 }
