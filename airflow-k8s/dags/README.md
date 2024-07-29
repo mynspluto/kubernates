336dc5e7cc3e3d6002747f5c02a0f7d2be21f9f1
mynspluto <mynspluto@gmail.com>
2024-07-29 14:32:34 +0900

    주가 하둡에 저장 카프카에 알림

--- airflow-k8s/dags/README.md
+++ airflow-k8s/dags/README.md
@@ -1,5 +1,6 @@
-pipenv install yfinance pandas => Pipfile, Pipfile.lock 생성됨(package.json 같은걸로 추측)
+맥에서 파이썬 코드 실행시 참고
+pipenv install yfinance pandas => npm install ~~와 비슷 Pipfile, Pipfile.lock 생성됨(package.json 같은걸로 추측)
 
-pipenv shell => 가상환경에서 node_module 같은걸 만드는것으로 추측
+pipenv shell => npm install? 가상환경에서 node_module 같은걸 만드는것으로 추측
 
 python fetch_stock_data.py
--- airflow-k8s/dags/fetch_stock_data.py
+++ airflow-k8s/dags/fetch_stock_data.py
@@ -1,16 +1,41 @@
 import yfinance as yf
 import pandas as pd
+from hdfs import InsecureClient
+from confluent_kafka import Producer
 
 # 주가 데이터를 수집할 종목 리스트
-tickers = ['AAPL', 'GOOGL', 'MSFT']
+tickers = ['^IXIC']
 
 # 주가 데이터를 저장할 함수
 def fetch_stock_data(ticker):
+    local_path = f'./{ticker}.csv'
     stock = yf.Ticker(ticker)
-    df = stock.history(period='1d')
+    df = stock.history(period='max')
     df.reset_index(inplace=True)
-    df.to_csv(f'./{ticker}.csv', index=False)
+    df.to_csv(local_path, index=False)
+    return local_path
 
-# PythonOperator를 사용하여 각 종목의 데이터를 수집
+def upload_to_hadoop(local_path, hdfs_path):
+    # HDFS 클라이언트 설정
+    client = InsecureClient('http://<hadoop-namenode-host>:<port>', user='your_username')
+
+    # 파일 업로드
+    client.upload(hdfs_path, local_path, overwrite=True)
+    
+def notify_kafka(ticker):
+    kafka_conf = {
+        'bootstrap.servers': '<kafka-broker-host>:<port>',
+        'client.id': 'stock-data-producer'
+    }
+    producer = Producer(kafka_conf)
+    
+    message = f'File for {ticker} has been uploaded to HDFS'
+    producer.produce('stock-data-topic', message)
+    producer.flush()
+
+# 각 종목의 데이터를 수집하고 HDFS에 업로드
 for ticker in tickers:
-    fetch_stock_data(ticker)
+    local_path = fetch_stock_data(ticker)
+    hdfs_path = f'/user/your_username/stock_data/{ticker}.csv'
+    #upload_to_hadoop(local_path, hdfs_path)
+    #notify_kafka(ticker)
