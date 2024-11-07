64ae2898eb376dd34b6fca1cb242c455c5d3da42
mynspluto <mynspluto@gmail.com>
2024-11-08 01:59:27 +0900

    hadoop dockerfile 작성, 구동 성공

--- all-in-one/hadoop/config/hadoop-env.sh
+++ all-in-one/hadoop/config/hadoop-env.sh
@@ -1,7 +1,7 @@
 export LANG=en_US.UTF-8
 export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-arm64"
+export JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"
 export HDFS_NAMENODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
 export HDFS_DATANODE_USER=root
