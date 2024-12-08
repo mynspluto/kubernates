fa08f37f476b931f8811871750d8426e2e2e7921
mynspluto <mynspluto@gmail.com>
2024-12-08 23:15:16 +0900

    web hdfs 로컬에서 테스트 성공 hadoop-test2.sh

--- all-in-one2/hadoop-test2.sh
+++ all-in-one2/hadoop-test2.sh
@@ -1,8 +1,13 @@
-#kubectl exec -it hadoop-0 -n hadoop -- hdfs dfs -rm /airflow/test_data/test_file.txt
+nohup kubectl port-forward svc/hadoop-service 9864:9864 -n hadoop > port-forward.log 2>&1 &
+#curl -i 'http://localhost:9870/webhdfs/v1/?op=LISTSTATUS'
 
-#kubectl cp hadoop/test_file.txt hadoop-0:/tmp/test_file.txt -n hadoop
+# 403
+curl -i -X PUT 'http://localhost:9870/webhdfs/v1/airflow/test_data?op=MKDIRS'
 
-#kubectl exec -it hadoop-0 -n hadoop -- hdfs dfs -mkdir -p /airflow/test_data
-curl -i -L -X PUT -T ./hadoop/test_file.txt 'http://localhost:9870/webhdfs/v1/airflow/test_data/test_file.txt?op=CREATE&overwrite=true'
-#kubectl exec -it hadoop-0 -n hadoop -- hdfs dfs -ls /airflow/test_data/
-#kubectl exec -it hadoop-0 -n hadoop -- hdfs dfs -cat /airflow/test_data/test_file.txt
+
+# kubectl exec -it hadoop-0 -- /bin/bash
+# bash-4.2$ cd bin/
+# bash-4.2$ hdfs dfs -chmod -R 777 /
+curl -i -X PUT -T ./hadoop/test_file.txt 'http://localhost:9864/webhdfs/v1/airflow/test_data/test_file.txt?op=CREATE&namenoderpcaddress=localhost:9000&overwrite=true'
+
+
